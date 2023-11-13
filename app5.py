f8a065bb2179264c1a135c8f21f1d6f8c0960239
silverwolf-x <185666132@qq.com>
2023-11-13 23:08:15 +0800

    Remove redundant code

--- app5.py
+++ app5.py
@@ -1,23 +1,18 @@
 # 第 3 章：模板
 from flask import Flask, render_template
-from sqlalchemy import create_engine,text
 from flask_sqlalchemy import SQLAlchemy
-import pandas as pd
-
 
 app = Flask(__name__)
 
+# import pandas as pd
 # engine = create_engine('mysql+pymysql://root:20190218@127.0.0.1:3306/test')
 # sql = 'select * from jobs'
 # print(pd.read_sql(sql, con))
+
 db_name = 'flasktest'
 app.config['SQLALCHEMY_DATABASE_URI'] = f'mssql+pymssql://sa:123456@DESKTOP-4KEIUAR/{db_name}?charset=utf8'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # 关闭对模型修改的监控
 db = SQLAlchemy(app) 
-
-'''
-创建外键后，要在原表关联起来 
-'''
 
 class User(db.Model):  # 表名将会是 user（自动生成，小写处理）
     # __tablename__ = "User"
@@ -66,7 +61,6 @@ def index():
     user = User.query.first()  # 读取用户记录
     movies = Movie.query.all()  # 读取所有电影记录
     return render_template('index.html', user=user, movies=movies)
-
 
 if __name__ == "__main__":
     with app.app_context():
